f7993f0753c87e159df69b66f80b026d91601446
Robert Connolly <robertoconnolly100@gmail.com>
2022-12-12 23:26:18 +0100

    imprimir tablas

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ class Juego:
         self.titulo_ventana = titulo_ventana
         self.dimensiones = dimensiones_ventana
         self.running = True
+        self.dimensiones_cuadro = dimensiones_ventana[0]/3
+        self.cuadro = [["x", "0", "0"],
+                       ["0", "0", "0"],
+                       ["0", "0", "0"]]
 
 
     def Inicio_Pygame(self):
@@ -30,6 +34,44 @@ class Juego:
         except:
             print(" Error Color de fondo invalido tiene que ser un tupla asi: (255, 0, 0)")
 
+        #Creando Cuadricula
+        self.Crear_Cuadricula()
+        self.Imprimir_Tabla()
+
+    def Crear_Cuadricula(self):
+        #Crear Cuadro
+        ancho = 0
+        alto = 0
+        for h in range(3):
+
+            for w in range(3):
+                pygame.draw.rect(self.pantalla, (255, 0, 0), (ancho, alto, self.dimensiones_cuadro, self.dimensiones_cuadro), 2)
+                ancho = ancho+self.dimensiones_cuadro+1
+            ancho = 0
+            alto = alto+self.dimensiones_cuadro+1
+
+    def Imprimir_Tabla(self):
+        ancho = 0
+        alto = 0
+        fila = 0
+        columna = 0
+        for i in range(3):
+            for a in range(3):
+                #print(self.cuadro[fila][columna])
+                print(int(ancho), int(alto))
+                pygame.draw.circle(self.pantalla, (255, 0, 0), (ancho+165, alto+165), 10, 3)
+                ancho = ancho+self.dimensiones_cuadro+1
+
+
+                columna = columna+1
+            ancho = 0
+            alto = alto+self.dimensiones_cuadro+1
+
+            fila = fila+1
+            columna = 0
+            
+
+
     def Bucle_Juego(self):
         pygame.display.flip()
         # Comprobamos que se de al boton de cerrar ventana si es asi cerramos juego
@@ -37,6 +79,7 @@ class Juego:
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     self.running = False
+
 
 
 
