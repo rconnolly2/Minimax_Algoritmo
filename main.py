10cafb12abb6e98db30723f2386a87e7cef75e40
Robert Connolly <robertoconnolly100@gmail.com>
2022-12-12 21:04:15 +0100

    Tres en raya & Minimax

--- main.py
+++ main.py
@@ -1,21 +1,47 @@
 import pygame
 import cv2
 
-COLOR_FONDO = (255, 230, 255)
-running = True
 
-pantalla = pygame.display.set_mode((500, 500))
+class Juego:
+    def __init__(self, color_fondo: tuple, titulo_ventana: str, dimensiones_ventana: tuple):
+        """
+        color_fondo => El color que queremos en el tetris en RGB con este formato (255, 255, 255)
 
-pygame.display.set_caption("3 en raya Minimax")
+        titulo_ventana => El nombre que queremos dar a la ventana
 
-pantalla.fill(COLOR_FONDO)
+        dimensiones_ventana => Las dimensiones que queremos dar a la ventana en este formato "2 Int": (500, 500)
+        """
+        self.COLOR_FONDO = color_fondo
+        self.titulo_ventana = titulo_ventana
+        self.dimensiones = dimensiones_ventana
+        self.running = True
 
-#Actualizar pantalla seria como SDL Display
-pygame.display.flip()
+
+    def Inicio_Pygame(self):
+        # titulo de ventana
+        pygame.display.set_caption(self.titulo_ventana)
+        try:
+            self.pantalla = pygame.display.set_mode(self.dimensiones)
+        except:
+            print("Error Valores de color_fondo al crear el objeto juego invalido: tiene que ser 2 int (500, 500)")
+
+        try:
+            self.pantalla.fill(self.COLOR_FONDO)
+        except:
+            print(" Error Color de fondo invalido tiene que ser un tupla asi: (255, 0, 0)")
+
+    def Bucle_Juego(self):
+        pygame.display.flip()
+        # Comprobamos que se de al boton de cerrar ventana si es asi cerramos juego
+        while self.running:
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    self.running = False
 
 
-while running:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            running = False
 
+Tetris = Juego((255, 255, 0), ("Mi juego de tetris"), (500, 500))
+
+Tetris.Inicio_Pygame()
+
+Tetris.Bucle_Juego()
