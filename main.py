47ee9799dea1962d61afc86ff9b9838108d2d5a6
Robert Connolly <robertoconnolly100@gmail.com>
2022-12-17 01:48:34 +0100

    detectar_raton

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Juego:
         self.running = True
         self.dimensiones_cuadro = dimensiones_ventana[0]/3
         self.cuadro = [["x", "0", "0"],
-                       ["0", "0", "0"],
+                       ["0", "o", "0"],
                        ["0", "0", "0"]]
 
 
@@ -53,23 +53,33 @@ class Juego:
     def Imprimir_Tabla(self):
         ancho = 0
         alto = 0
-        fila = 0
-        columna = 0
+        suma_up = pygame.Rect(((ancho+82)-15), alto+30, 30, 100)
+        suma_down = pygame.Rect(((ancho+82)-50), alto+65, 100, 30)
+
         for i in range(3):
             for a in range(3):
-                #print(self.cuadro[fila][columna])
-                print(int(ancho), int(alto))
-                pygame.draw.circle(self.pantalla, (255, 0, 0), (ancho+165, alto+165), 10, 3)
+                print(self.cuadro[a][i])
+                #Comprobamos que si en caso que en la lista sea x pintamos x si es circulo circulo :
+                if (self.cuadro[a][i] == "x"):
+                    pygame.draw.rect(self.pantalla, (255, 0, 0), suma_up)
+                    pygame.draw.rect(self.pantalla, (255, 0, 0), suma_down)
+                elif (self.cuadro[a][i] == "o"):
+                    pygame.draw.circle(self.pantalla, (255, 0, 0), (ancho+82, alto+82), 30, 3)
+
                 ancho = ancho+self.dimensiones_cuadro+1
 
-
-                columna = columna+1
             ancho = 0
             alto = alto+self.dimensiones_cuadro+1
 
-            fila = fila+1
-            columna = 0
-            
+    def Detectar_Raton(self):
+        botones_ratones = pygame.mouse.get_pressed(num_buttons=3)
+        #Si el boton izquierdo del raton se presiona devolvemos x, y
+        if botones_ratones[0] == True:
+            pos_raton = pygame.mouse.get_pos()
+            return pos_raton
+
+    def Raton_Dentro(self):
+        None
 
 
     def Bucle_Juego(self):
@@ -79,6 +89,9 @@ class Juego:
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
                     self.running = False
+
+            #Aqui debajo va el codigo del bucle del juego
+            print(self.Detectar_Raton())
 
 
 
